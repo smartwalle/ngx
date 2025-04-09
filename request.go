b2f09fef6ceeee752d4acbfef9c8fe68badf977e
SmartWalle <smartwalle@gmail.com>
2025-04-09 10:53:31 +0800

    注释

--- request.go
+++ request.go
@@ -34,28 +34,28 @@ const (
 )
 
 type Request struct {
-	body        io.Reader
 	client      *http.Client
 	header      http.Header
-	uQuery      url.Values
+	body        io.Reader
+	method      string
+	rawURL      string
+	rawQuery    url.Values
 	query       url.Values
 	form        url.Values
 	files       FormFiles
-	Method      string
-	target      string
 	contentType ContentType
 	cookies     []*http.Cookie
 }
 
-func NewRequest(method, target string, opts ...Option) *Request {
-	var nURL, _ = url.Parse(target)
+func NewRequest(method, rawURL string, opts ...Option) *Request {
+	var nURL, _ = url.Parse(rawURL)
 	var req = &Request{}
-	req.Method = strings.ToUpper(method)
-	req.target = target
+	req.method = strings.ToUpper(method)
+	req.rawURL = rawURL
 	req.contentType = ContentTypeURLEncode
 
 	if nURL != nil {
-		req.uQuery = nURL.Query()
+		req.rawQuery = nURL.Query()
 	}
 
 	for _, opt := range opts {
@@ -71,22 +71,32 @@ func NewRequest(method, target string, opts ...Option) *Request {
 	return req
 }
 
+// TrimURLQuery 清除 URL 中原有的查询参数信息
 func (r *Request) TrimURLQuery() {
-	r.uQuery = nil
+	r.rawQuery = nil
 }
 
+// SetContentType 设置 Content-Type
 func (r *Request) SetContentType(contentType ContentType) {
 	r.contentType = contentType
 }
 
+// SetBody 设置请求体
+//
+// 如果同时设置了 Body 和 Form，Body 的优先级则高于 Form，Form 中的信息将被舍弃。
 func (r *Request) SetBody(body io.Reader) {
 	r.body = body
 }
 
+// SetForm 设置请求参数（表单）
 func (r *Request) SetForm(form url.Values) {
 	r.form = form
 }
 
+// Form 获取请求参数（表单）
+//
+// 对于 POST 请求，该参数将通过 Body 传递；
+// 对于 GET 一类的请求，该参数将拼接在 URL 的查询参数中。
 func (r *Request) Form() url.Values {
 	if r.form == nil {
 		r.form = url.Values{}
@@ -94,10 +104,14 @@ func (r *Request) Form() url.Values {
 	return r.form
 }
 
+// SetQuery 设置查询参数信息
+//
+// 该参数将拼接在 URL 的查询参数中。
 func (r *Request) SetQuery(query url.Values) {
 	r.query = query
 }
 
+// Query 获取查询参数信息
 func (r *Request) Query() url.Values {
 	if r.query == nil {
 		r.query = url.Values{}
@@ -105,10 +119,12 @@ func (r *Request) Query() url.Values {
 	return r.query
 }
 
+// SetHeader 设置请求头
 func (r *Request) SetHeader(header http.Header) {
 	r.header = header
 }
 
+// Header 获取请求头
 func (r *Request) Header() http.Header {
 	if r.header == nil {
 		r.header = http.Header{}
@@ -116,15 +132,17 @@ func (r *Request) Header() http.Header {
 	return r.header
 }
 
+// SetFileForm 设置上传文件信息
+func (r *Request) SetFileForm(files FormFiles) {
+	r.files = files
+}
+
+// FileForm 获取上传文件信息
 func (r *Request) FileForm() FormFiles {
 	if r.files == nil {
 		r.files = FormFiles{}
 	}
 	return r.files
-}
-
-func (r *Request) SetFileForm(files FormFiles) {
-	r.files = files
 }
 
 func (r *Request) AddCookie(cookie *http.Cookie) {
@@ -139,14 +157,14 @@ func (r *Request) Do(ctx context.Context) (*http.Response, error) {
 	var req *http.Request
 	var err error
 	var body io.Reader
-	var toQuery bool
+	var mergeToRawQuery bool
 
-	if r.Method == http.MethodGet ||
-		r.Method == http.MethodTrace ||
-		r.Method == http.MethodOptions ||
-		r.Method == http.MethodHead ||
-		r.Method == http.MethodDelete {
-		toQuery = true
+	if r.method == http.MethodGet ||
+		r.method == http.MethodTrace ||
+		r.method == http.MethodOptions ||
+		r.method == http.MethodHead ||
+		r.method == http.MethodDelete {
+		mergeToRawQuery = true
 	}
 
 	if r.body != nil {
@@ -172,16 +190,16 @@ func (r *Request) Do(ctx context.Context) (*http.Response, error) {
 
 		r.SetContentType(ContentType(bodyWriter.FormDataContentType()))
 		body = bodyBuffer
-	} else if len(r.form) > 0 && !toQuery {
+	} else if len(r.form) > 0 && !mergeToRawQuery {
 		body = strings.NewReader(r.form.Encode())
 	}
 
-	req, err = http.NewRequestWithContext(ctx, r.Method, r.target, body)
+	req, err = http.NewRequestWithContext(ctx, r.method, r.rawURL, body)
 	if err != nil {
 		return nil, err
 	}
 
-	var rawQuery = r.uQuery
+	var rawQuery = CloneValues(r.rawQuery)
 	if rawQuery == nil {
 		rawQuery = url.Values{}
 	}
@@ -192,7 +210,7 @@ func (r *Request) Do(ctx context.Context) (*http.Response, error) {
 		}
 	}
 
-	if toQuery {
+	if mergeToRawQuery {
 		for key, values := range r.form {
 			for _, value := range values {
 				rawQuery.Add(key, value)
